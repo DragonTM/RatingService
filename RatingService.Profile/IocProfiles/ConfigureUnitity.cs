376ef701aefca98a6556197787c1427ffada5654
Ivan_Kushnir <ivan_kushnir@epam.com>
2016-05-25 21:15:07 +0300

    add profile proj

---
+++ RatingService.Profile/IocProfiles/ConfigureUnitity.cs
@@ -0,0 +1,38 @@
+﻿using System.Data.Entity;
+using Microsoft.Practices.Unity;
+using RaitngService.Bll.Services.Implementations;
+using RaitngService.Bll.Services.Interfaces;
+using RatingService.Auth.Implementation;
+using RatingService.Auth.Interfaces;
+using RatingService.Dal;
+using RatingService.Dal.Implementation;
+using RatingService.Dal.Interfaces;
+
+namespace RatingService.Profile.IocProfiles
+{
+	public class ConfigureUnitity
+	{
+		public void Init(IUnityContainer container)
+		{
+			InitDal(container);
+			InitBll(container);
+			InitAuth(container);
+		}
+
+		private void InitDal(IUnityContainer container)
+		{
+			container.RegisterType<DbContext, RatingServiceDbContext>(new PerResolveLifetimeManager());
+			container.RegisterType(typeof(IRepository<>), typeof(GenericRepository<>));
+		}
+
+		private void InitBll(IUnityContainer container)
+		{
+			container.RegisterType<IProtectingService, ProtectingService>();
+		}
+
+		private void InitAuth(IUnityContainer container)
+		{
+			container.RegisterType<IAuthService, AuthService>();
+		}
+	}
+}
